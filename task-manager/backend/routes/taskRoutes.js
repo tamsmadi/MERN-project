477b2d1cc27ba47ddb9535f0b01bdc20a1a9476b
tamsmadi <thamer.smadi@outlook.com>
2025-05-29 11:33:26 +0300

    commit

--- task-manager/backend/routes/taskRoutes.js
+++ task-manager/backend/routes/taskRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Task = require('../../models/task.js');
+const Task = require('../models/task.js');
 
 // Get all tasks
 router.get('/', async (req, res) => {
